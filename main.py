b6f90f6611dff5db58bfb42be0f6cbaf7591be48
EnchiladaStyle <jon19073@byui.edu>
2024-07-19 21:03:36 -0600

    Cleaned up some code

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import wx
 import os
-from createDispatch import createDispatch
 from MasterTemplate import generateTemplate
 from loadTemplate import selectTemplates, loadTemplate
 from FileDropTarget import FileDropTarget
