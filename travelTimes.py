3997a5e3c216b71a4c5b1211a1b170e1a80a6146
EnchiladaStyle <jon19073@byui.edu>
2024-08-09 11:05:15 -0600

    cleaned up some code

--- travelTimes.py
+++ travelTimes.py
@@ -1,5 +1,4 @@
 jeepBaseTimes = {
-    "Dock 3": dock3Times["Jeep Base"],
     "Dock 4": dock4Times["Jeep Base"],
     "Lumberjack": lumberjackTimes["Jeep Base"],
     "George Inlet": georgeInletTimes["Jeep Base"],
