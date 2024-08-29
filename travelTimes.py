31f0125c5b0a54a04ae8267a95dbf544606b1630
EnchiladaStyle <jon19073@byui.edu>
2024-08-29 17:35:50 -0600

    Cleaned up some code

--- travelTimes.py
+++ travelTimes.py
@@ -1,6 +1,5 @@
 
 harriotHuntTimes = {
-    "Cape Fox Lodge": cfLodgeTimes["Harriot Hunt"],
     "Saxmon": saxmonTimes["Harriot Hunt"],
     "Disco": discoTimes["Harriot Hunt"],
     "Taquan": taquanTimes["Harriot Hunt"],
