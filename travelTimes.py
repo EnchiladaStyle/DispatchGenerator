ff3550d0ab8d7732f3ec33b2b90d78b76337db16
EnchiladaStyle <jon19073@byui.edu>
2024-09-04 20:59:35 -0600

    Cleaned up some code

--- travelTimes.py
+++ travelTimes.py
@@ -1,6 +1,5 @@
 
 whiteRiverTimes = {
-    "Ward Cove": wardCoveTimes["White River"],
     "Dock 2": dock2Times["White River"],
     "Dock 1": dock1Times["White River"],
     "Dock 3": dock3Times["White River"],
